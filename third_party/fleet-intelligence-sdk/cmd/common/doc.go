1b4538d32f5325ed0feac345ca31e5772c60d941
Jingxiang Zhang <jingzhang@nvidia.com>
2026-04-10 10:16:04 -0700

    chore: prune unused vendored sdk packages
    Signed-off-by: Jingxiang Zhang <jingzhang@nvidia.com>

--- third_party/fleet-intelligence-sdk/cmd/common/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2024 Lepton AI Inc
-// Source: https://github.com/leptonai/gpud
-
-// Package common provides common utilities for the gpud command.
-package common
--- third_party/fleet-intelligence-sdk/components/accelerator/nvidia/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2024 Lepton AI Inc
-// Source: https://github.com/leptonai/gpud
-
-// Package nvidia contains the NVIDIA accelerator components
-// and its query interface.
-package nvidia
